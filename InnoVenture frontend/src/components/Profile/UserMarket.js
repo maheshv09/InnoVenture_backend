f67dbb374c20217d98d7dc70cce1c9361224f9df
Mahesh Kishore Vaswani <118044178+maheshv09@users.noreply.github.com>
2024-09-14 21:48:02 +0530

    Updated folder structure

--- InnoVenture frontend/src/components/Profile/UserMarket.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserMarket = () => {
-  return (
-    <div>UserMarket</div>
-  )
-}
-
-export default UserMarket
--- InnoVenture frontend/src/helper.js
+++
@@ -1,2 +0,0 @@
-export const stripePublicKey =
-  "pk_test_51Ob5WQSFnWuruuiR1g7avSwxACKgOmYua2kx0bUfIacvv96crjGafpsGbCWIdQEyqLinBMmOrSQT88KVFmCGIp2f00fCyvcLgP";
